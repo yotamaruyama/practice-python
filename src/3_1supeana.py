40c18bc05121d5eb15dd1e544928ffb08c058961
yotamaruyama <maruyama.youta2002@gmail.com>
2023-06-21 09:02:06 +0000

    Peaksを表示する

--- src/3_1supeana.py
+++ src/3_1supeana.py
@@ -1,24 +1,46 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 # CSVファイルを読み込む
-df = pd.read_csv(
-    "TRACE003.CSV",
-    skiprows=11,
-    encoding="shift-jis",
-)  # 'your_file.csv'はあなたのCSVファイルのパスに置き換えてください
+df = pd.read_csv("TRACE003.CSV", skiprows=11, encoding="shift-jis")
 
 # 'Frequency'と'Amplitude'列を抽出
 data = df[["Frequency", "Amplitude"]]
-# data["Frequency"] = data["Frequency"].astype(float)
 data["Frequency"] = data["Frequency"].str.replace("Hz", "").astype(float)
 data["Amplitude"] = data["Amplitude"].str.replace("dBm", "").astype(float)
 
+# データ列ごとに分割
+frequency = data["Frequency"].values  # 周波数
+amplitude = data["Amplitude"].values  # 振幅
 
-# データをグラフ化
-plt.plot(data["Frequency"], data["Amplitude"])
-plt.xlabel("Frequency (Hz)")
-plt.ylabel("Amplitude (dBm)")
-plt.title("Spectrum Analyzer Data")
-plt.grid(True)
+# プロットする周波数範囲（基本周波数と奇数倍のピークのみ）
+base_frequency = 1e6  # 基本周波数（1MHz）
+max_frequency = frequency[-1]  # 計測周波数の最大値
+plot_frequency = np.arange(
+    base_frequency,
+    max_frequency + 1,
+    2 * base_frequency,
+)  # 基本周波数の奇数倍
+
+# 基本周波数と奇数倍のピークのインデックスを取得
+peak_indices = []
+for f in plot_frequency:
+    index = np.abs(frequency - f).argmin()  # 最も近い周波数のインデックスを取得
+    peak_indices.append(index)
+
+# プロット
+plt.plot(frequency, amplitude, label="Spectrum")
+plt.scatter(
+    frequency[peak_indices],
+    amplitude[peak_indices],
+    color="red",
+    label="Peaks",
+)
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Amplitude [dBm]")
+plt.legend()
 plt.show()
+# ピークの周波数と振幅を表示
+for i in peak_indices:
+    print(f"Frequency: {frequency[i]} Hz, Amplitude: {amplitude[i]} dBm")
